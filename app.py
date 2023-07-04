fe45da232ffccca10d9046699b6bc304c6e64b71
Ayush Gupta <ayush2020cs159@abesit.edu.in>
2023-07-05 04:29:08 +0530

    flask-login added
    Added Flask_Login support for session management and to access and use different table for different users and added more security features

--- app.py
+++ app.py
@@ -1,23 +1,17 @@
-'''
- ToDo app with authentication in Flask framework. I have to use Flask and SQLAlchemy extensions to handle user sessions and database operations. I have to also add some basic validations and error handling. The app should has the following endpoints:
-
-/api/todo/register: To register a new user with username and password
-/api/todo/login: To log in an existing user with username and password
-/api/todo/logout: To log out the current user
-/api/todo: To get all the todo items for the current user
-/api/todo/<int:id>: To get, update or delete a specific todo item for the current user
-The app uses a SQLite database to store the user and todo data. You can change it to any other database by modifying the SQLALCHEMY_DATABASE_URI configuration.'''
-
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
 app.config['SECRET_KEY'] = 'secret_key'
 db = SQLAlchemy(app)
 
-#Define the users table model
-class User(db.Model):
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# Define the users table model
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
@@ -25,7 +19,11 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
-#Route for the user registration
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+# Route for the user registration
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
@@ -53,7 +51,7 @@ def signup():
     return render_template('signup.html')
 
 
-#Route for the user login
+# Route for the user login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -63,6 +61,7 @@ def login():
             # Get the user from the database
             user = User.query.filter_by(username=request.form['username']).first()
             if user and user.password == request.form['password']:
+                login_user(user)
                 flash('Login successful')
                 return redirect(url_for('show_all'), code=307)
             else:
@@ -72,44 +71,50 @@ def login():
     # Render the login form
     return render_template('login.html')
 
-#Route for the user logout
+# Route for the user logout
 @app.route('/logout')
+@login_required
 def logout():
+    logout_user()
     flash('Logout successful')
     return redirect(url_for('login'))
 
 
 # Define the tasks table model
-class Tasks(db.Model):
-    id = db.Column('task_id', db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    description = db.Column(db.String(200))
-    status = db.Column(db.String(10))
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(120), nullable=False)
+    status = db.Column(db.String(20), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-    def __init__(self, name, description, status):
+    def __init__(self, name, description, status, user_id):
         self.name = name
         self.description = description
         self.status = status
+        self.user_id = user_id
 
-#Route for displays home page
+# Route for displaying home page
 @app.route('/home')
 def home():
     return render_template('home.html')
 
+
 # Route for displaying all tasks
 @app.route('/api/todo', methods=['GET', 'POST'])
+@login_required
 def show_all():
     if request.method == 'POST':
-        with app.app_context():
-            tasks = Tasks.query.all()
-            print(tasks)
-            return render_template('show_all.html', tasks=tasks)
+        # Retrieve all tasks for the current user
+        tasks = Task.query.filter_by(user_id=current_user.id).all()
+        return render_template('show_all.html', tasks=tasks)
     else:
         return "Method not allowed", 405
 
 
 # Route for adding a new task
 @app.route('/api/todo/create', methods=['GET', 'POST'])
+@login_required
 def new():
     # Handle form submission
     if request.method == 'POST':
@@ -117,8 +122,8 @@ def new():
         if not request.form['name'] or not request.form['description'] or not request.form['status']:
             flash('Please enter all the fields', 'error')
         else:
-            # Create a new task object
-            task = Tasks(request.form['name'], request.form['description'], request.form['status'])
+            # Create a new task object for the current user
+            task = Task(request.form['name'], request.form['description'], request.form['status'], current_user.id)
 
             with app.app_context():
                 # Add the task to the database
@@ -134,9 +139,15 @@ def new():
 
 # Route for editing a task
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
+@login_required
 def edit(id):
     # Get the task from the database
-    task = Tasks.query.get(id)
+    task = Task.query.filter_by(id=id, user_id=current_user.id).first()
+
+    if not task:
+        # If the task is not found, redirect to the show_all() function with an error message
+        flash('Invalid task ID')
+        return redirect(url_for('show_all'))
 
     # Handle form submission
     if request.method == 'POST':
@@ -144,11 +155,10 @@ def edit(id):
         if not request.form['name'] or not request.form['description'] or not request.form['status']:
             flash('Please enter all the fields', 'error')
         else:
-            form_id = request.form['id']
             name = request.form['name']
             description = request.form['description']
             status = request.form['status']
-            # print(f"Received form data: id={form_id}, name={name}, description={description}, status={status}")
+
             # Update the task object
             task.name = name
             task.description = description
@@ -158,11 +168,6 @@ def edit(id):
                 # Update the task in the database
                 db.session.merge(task)
                 db.session.commit()
-                # print(f"Updated task record: id={form_id}, name={name}, description={description}, status={status}")
-                # Check if the transaction was committed
-                if not db.session.is_active:
-                    print("\n Transaction was not committed")
-
 
             flash('Record was successfully updated')
             return redirect(url_for('show_all'), code=307)
@@ -170,26 +175,29 @@ def edit(id):
     # Render the edit task form
     return render_template('edit.html', task=task)
 
+# Route for deleting a task
+@app.route('/delete/<int:id>', methods=['POST'])
+@login_required
+def delete(id):
+    # Get the task from the database
+    task = Task.query.filter_by(id=id, user_id=current_user.id).first()
 
-# route for delete a task
-@app.route('/delete', methods=['POST'])
-def delete():
-    task_id = request.form['id']
-    task = Tasks.query.get(task_id)
-    if task:
-        db.session.delete(task)
-        db.session.commit()
-        flash('Record was successfully deleted')
-    else:
-        flash('Task record not found', 'error')
+    if not task:
+        # If the task is not found, redirect to the show_all() function with an error message
+        flash('Invalid task ID')
+        return redirect(url_for('show_all'), code=307)
+
+    # Delete the task from the database
+    db.session.delete(task)
+    db.session.commit()
+
+    flash('Record was successfully deleted')
     return redirect(url_for('show_all'), code=307)
 
 if __name__ == '__main__':
     with app.app_context():
         # Create the database tables if they don't exist
         db.create_all()
-        
 
     # Start the Flask application
     app.run(debug=True)
-
